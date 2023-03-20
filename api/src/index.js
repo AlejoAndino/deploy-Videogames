9ba3630d999926df8c05ec80b64a0757407584be
AleAndino <aleandino.cba@gmail.com>
2023-03-20 09:28:28 -0300

    refactoring

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,16 @@
 const app = require('./routes/server');
 const { sequelize } = require('./DB_connection');
 const { getAllGenres } = require('./controllers/getAllGenres');
+require('dotenv').config();
+const { PORT } = process.env;
 
 
 sequelize.sync({force: true}).then( async () =>{
     console.log('DB successfully connected');
     // console.log("Soy index.js", await saveApiData());
     // await saveApiData()
-    app.listen(3001, () => {
-        console.log("Server on port 3001");
+    app.listen(PORT, () => {
+        console.log(`Server on port ${PORT}`);
     })
 }).catch((error)=> {
     console.log(error);
