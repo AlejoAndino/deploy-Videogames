430bb66ac011891c4a0a2f4d96b37b6d07341686
AleAndino <aleandino.cba@gmail.com>
2023-03-20 09:48:51 -0300

    refactoring

--- api/src/DB_connection.js
+++ api/src/DB_connection.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const videogameModel  = require('./models/Videogame');
 const genreModel = require('./models/Genre');
 
 
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
    { 
       logging: false , native: false
    }
